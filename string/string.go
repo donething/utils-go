3f0e55184f4b31278c2f6fb75651f7ba4bf51dbb
Donething <donething.net@gmail.com>
2018-11-11 01:00:35 +0800

    1. 添加字符串工具：UTF-8和GBK转换；获得日期格式化后的字符串

---
+++ string/string.go
@@ -0,0 +1,41 @@
+package string
+
+import (
+	"bytes"
+	"golang.org/x/text/encoding/simplifiedchinese"
+	"golang.org/x/text/transform"
+	"io/ioutil"
+	"strings"
+	"time"
+)
+
+// 编码转换：http://mengqi.info/html/2015/201507071345-using-golang-to-convert-text-between-gbk-and-utf-8.html
+
+// GBK编码转UTF-8
+func GBKToUTF8(s []byte) ([]byte, error) {
+	reader := transform.NewReader(bytes.NewReader(s), simplifiedchinese.GBK.NewDecoder())
+	d, e := ioutil.ReadAll(reader)
+	if e != nil {
+		return nil, e
+	}
+	return d, nil
+}
+
+// UTF-8编码转GBK
+func UTF8ToGBK(s []byte) ([]byte, error) {
+	reader := transform.NewReader(bytes.NewReader(s), simplifiedchinese.GBK.NewEncoder())
+	d, e := ioutil.ReadAll(reader)
+	if e != nil {
+		return nil, e
+	}
+	return d, nil
+}
+
+// 格式化时间
+// 如果format为空白字符，则默认设为"2006-01-02 15:04:05"
+func FormatDate(t time.Time, format string) string {
+	if strings.TrimSpace(format) == "" {
+		format = "2006-01-02 15:04:05"
+	}
+	return t.Format(format)
+}
