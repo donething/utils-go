8ec352c4e501f96e8a73bb82beea8e0f6bf2eb2a
donething <donething.net@gmail.com>
2019-01-08 01:48:09 +0800

    1. 添加base64编码、解码函数

--- dostr/string.go
+++ dostr/string.go
@@ -2,16 +2,16 @@ package dostr
 
 import (
 	"bytes"
+	"encoding/base64"
 	"golang.org/x/text/encoding/simplifiedchinese"
 	"golang.org/x/text/transform"
 	"io/ioutil"
 	"time"
 )
 
-// 编码转换：http://mengqi.info/html/2015/201507071345-using-golang-to-convert-text-between-gbk-and-utf-8.html
-
 // GBK编码转UTF-8
 func GBKToUTF8(s []byte) ([]byte, error) {
+	// 编码转换：http://mengqi.info/html/2015/201507071345-using-golang-to-convert-text-between-gbk-and-utf-8.html
 	reader := transform.NewReader(bytes.NewReader(s), simplifiedchinese.GBK.NewDecoder())
 	d, e := ioutil.ReadAll(reader)
 	if e != nil {
@@ -37,4 +37,19 @@ func FormatDate(t time.Time, format string) string {
 		format = "2006-01-02 15:04:05"
 	}
 	return t.Format(format)
+}
+
+// base64编码
+func Base64Encode(str string) string {
+	// https://stackoverflow.com/a/28672789/8179418
+	return base64.StdEncoding.EncodeToString([]byte(str))
+}
+
+// base64解码
+func Base64Decode(str string) ([]byte, error) {
+	data, err := base64.StdEncoding.DecodeString(str)
+	if err != nil {
+		return nil, err
+	}
+	return data, nil
 }
