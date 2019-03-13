9c29be0ec29643b517842e00dd531996b39e46aa
donething <donething.net@gmail.com>
2019-03-13 20:45:08 +0800

    1. 增加微信测试号消息推送

---
+++ wxpush/sandbox_push_test.go
@@ -0,0 +1,34 @@
+package wxpush
+
+import (
+	"github.com/donething/utils-go/wxpush/entity"
+	"log"
+	"testing"
+)
+
+func TestNewSandbox(t *testing.T) {
+	wx, err := NewSandbox("", "")
+	if err != nil {
+		t.Log(err)
+	}
+	log.Printf("%v", wx)
+
+	// 第二个测试
+	wx, err = NewSandbox("wxfbc379f966f4a234", "59be39dfcf4e95be0989748dad3e0ff6")
+	if err != nil {
+		t.Log(err)
+	}
+	log.Printf("%v", wx)
+}
+
+func TestWXSendbox_Push(t *testing.T) {
+	wx, err := NewSandbox("wxfbc379f966f4a234", "59be39dfcf4e95be0989748dad3e0ff6")
+	if err != nil {
+		t.Fatal(err)
+	}
+	success, res, err := wx.PushTpl(entity.PushMsg{})
+	if err != nil {
+		t.Fatal(err)
+	}
+	t.Log(success, res)
+}
