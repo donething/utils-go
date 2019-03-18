bb0a96ba5f34eb6f62fdf9a0a4da26ed65d26c45
donething <donething.net@gmail.com>
2019-03-18 22:12:23 +0800

    1. 增加UTF-8和UTF-16互转函数

--- dostr/string.go
+++ dostr/string.go
@@ -3,10 +3,13 @@ package dostr
 import (
 	"bytes"
 	"encoding/base64"
+	"encoding/binary"
 	"golang.org/x/text/encoding/simplifiedchinese"
+	"golang.org/x/text/encoding/unicode"
 	"golang.org/x/text/transform"
 	"io/ioutil"
 	"time"
+	"unicode/utf16"
 )
 
 // GBK编码转UTF-8
@@ -28,6 +31,29 @@ func UTF8ToGBK(s []byte) ([]byte, error) {
 		return nil, e
 	}
 	return d, nil
+}
+
+// 将UTF16编码转换为UTF8编码
+// 参数order: binary.LittleEndian、binary.BigEndian等
+// 参考：https://gist.github.com/bradleypeabody/185b1d7ed6c0c2ab6cec#gistcomment-2780177
+func UTF16ToUTF8(bs []byte, order binary.ByteOrder) ([]rune, error) {
+	ints := make([]uint16, len(bs)/2)
+	if err := binary.Read(bytes.NewReader(bs), order, &ints); err != nil {
+		return nil, err
+	}
+	// 可通过string()将其转换为string
+	// 再通过[]byte()将string转为[]byte
+	return utf16.Decode(ints), nil
+}
+
+// 将UTF8编码转换为UTF16编码
+// 参数：endian: unicode.LittleEndian、unicode.LittleEndian
+// 参数：bom: unicode.IgnoreBOM、unicode.UseBOM、unicode.ExpectBOM
+// 参考：https://forum.golangbridge.org/t/how-to-convert-utf-8-string-to-utf-16-be-string/7072/2
+func UTF8ToUTF16(bs []byte, endian unicode.Endianness, bom unicode.BOMPolicy) ([]byte, error) {
+	decoder := unicode.UTF16(endian, bom).NewDecoder()
+	bs16, err := decoder.Bytes(bs)
+	return bs16, err
 }
 
 // 格式化时间
