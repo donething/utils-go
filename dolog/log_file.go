8b0627fac9e96445f6e36740c44a58474222c392
donething <donething.net@gmail.com>
2019-03-21 19:09:44 +0800

    1. 更改Log2File()为LogToFile()，并增加返回值lf以在程序结束前手动关闭打开的日志文件

--- dolog/log_file.go
+++ dolog/log_file.go
@@ -13,14 +13,15 @@ const (
 	LOG_FORMAT = log.LstdFlags | log.Lshortfile
 )
 
-// 设置将log保存到文件
-// openFlags为文件读写模式：dofile中WRITE_***
-func Log2File(logName string, openFlags int, logFormat int) (err error) {
+// 将log保存到文件
+// 参数：openFlags为文件读写模式：dofile中WRITE_***
+// 返回值：lf为日志文件的FileInfo，需要在程序结束时，手动调用 lf.close()
+func LogToFile(logName string, openFlags int, logFormat int) (lf *os.File, err error) {
 	// 打印log时显示时间戳
 	log.SetFlags(logFormat)
 
 	// 将日志输出到屏幕和日志文件
-	lf, err := os.OpenFile(logName, openFlags, 0644)
+	lf, err = os.OpenFile(logName, openFlags, 0644)
 	if err != nil {
 		return
 	}
@@ -30,5 +31,5 @@ func Log2File(logName string, openFlags int, logFormat int) (err error) {
 	// MultiWriter()的参数顺序也重要，如果使用"-H windowsgui"参数build，并且需要将日志保存到文件，
 	// 则需要将日志文件的指针（lf）放到os.Stdout之前，否则log不会产生输出
 	log.SetOutput(io.MultiWriter(lf, os.Stdout))
-	return nil
+	return
 }
